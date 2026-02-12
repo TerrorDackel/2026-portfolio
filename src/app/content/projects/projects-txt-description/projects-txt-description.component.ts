61bbf00df1cac84e20e53ef376ba1f94b0f0ac4e
Jennifer Thomas <jenny.codes.007@gmail.com>
2026-02-12 13:58:21 +0100

    Add comments JSDoc.

--- src/app/content/projects/projects-txt-description/projects-txt-description.component.ts
+++ src/app/content/projects/projects-txt-description/projects-txt-description.component.ts
@@ -2,17 +2,53 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslatePipe } from '@ngx-translate/core';
 
+/**
+ * Project text/description component.
+ *
+ * Responsibilities:
+ * - Renders the textual metadata for a single portfolio project.
+ * - Uses ngx-translate to render the project description from a translation key.
+ *
+ * Inputs:
+ * - name: project title shown in the UI.
+ * - usedProgramms: list of technologies used by the project.
+ * - description: translation key for the project description text.
+ * - gitLink: URL to the project's repository.
+ * - livetest: URL to the live demo.
+ */
 @Component({
-    selector: 'app-projects-txt-description',
-    standalone: true,
-    imports: [CommonModule, TranslatePipe],
-    templateUrl: './projects-txt-description.component.html',
-    styleUrl: './projects-txt-description.component.sass'
+  selector: 'app-projects-txt-description',
+  standalone: true,
+  imports: [
+    CommonModule,
+    TranslatePipe
+  ],
+  templateUrl: './projects-txt-description.component.html',
+  styleUrl: './projects-txt-description.component.sass'
 })
 export class ProjectsTxtDescriptionComponent {
-    @Input() name!: string;
-    @Input() usedProgramms!: string[]; 
-    @Input() description!: string;
-    @Input() gitLink!: string;
-    @Input() livetest!: string;
+  /**
+   * Project title shown in the UI.
+   */
+  @Input() name!: string;
+
+  /**
+   * List of technologies used by the project.
+   */
+  @Input() usedProgramms!: string[];
+
+  /**
+   * Translation key for the project description text.
+   */
+  @Input() description!: string;
+
+  /**
+   * URL to the project's source repository.
+   */
+  @Input() gitLink!: string;
+
+  /**
+   * URL to the project's live demo.
+   */
+  @Input() livetest!: string;
 }
